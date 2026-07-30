5c3c3a318e2da27964b9ae7ac4ec89b964cd0d08
Antoni <231758370+Ant-tenna@users.noreply.github.com>
2026-07-29 20:22:08 -0400

    Overwrite firmware sketch.ino

--- firmware/sketch.ino
+++ firmware/sketch.ino
@@ -1,15 +1,25 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64 
 
-#define SENSOR_PIN 18 
+#define SENSOR_PIN 18
 #define BTN_UP 32
 #define BTN_DOWN 33
+#define BTN_SWITCH 27
+
+#define AO_PHOTO 34
+#define DIG_PHOTO 26
+#define DHT11_PIN 4
+
+#define BUZZER 15
 
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+DHT dht11(DHT11_PIN, DHT11);
 
 const int totalLines = 10;
 const char* fanfiction[totalLines] = {
@@ -26,33 +36,37 @@ const char* fanfiction[totalLines] = {
 
 };
 
-const int MAXILINES = 6;
+const int MAXILINES = 5;
 int lineaActual = 0;
-bool pantallaApagada = false;
+bool modoPanic = false;
 
 bool lastStateUp = HIGH;
 bool lastStateDown = HIGH;
+bool lastStateSwitch = HIGH;
 
-void dibujarTexto(){
-  
-  oled.clearDisplay();
-  oled.setCursor(0, 0);
+void dibujarTexto();
+void dibujarPanic();
+void beep();
 
-  for(int i = 0; i < MAXILINES; i++){
-    int indice = lineaActual + i;
-    if(indice < totalLines){
-      oled.println(fanfiction[indice]);
-    }
-  }
- oled.display();
-}
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(SENSOR_PIN, INPUT);
+  //button
+  
   pinMode(BTN_UP, INPUT_PULLUP);
   pinMode(BTN_DOWN, INPUT_PULLUP);
+  pinMode(BTN_SWITCH, INPUT_PULLUP);
+
+  //led y buzzer
+  pinMode(BUZZER, OUTPUT);
+
+  //sensores
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(AO_PHOTO, INPUT);
+
+  dht11.begin();
+
 
   if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("failed to start SSD1306 OLED"));
@@ -64,30 +78,38 @@ void setup() {
   oled.setTextColor(WHITE); 
 
   dibujarTexto();
-
+  
   delay(2000);
 }
 
 void loop() {
 
+  bool buttonSwitch = digitalRead(BTN_SWITCH);
   int state = digitalRead(SENSOR_PIN);
 
-  if (state == LOW){
-    if(!pantallaApagada){
-      oled.clearDisplay();
-      oled.display();
-      pantallaApagada = true;
+  if (state == HIGH || (lastStateSwitch == HIGH && buttonSwitch == LOW)) {
+    if (!modoPanic) {
+      modoPanic = true;
+      dibujarPanic();
+      beep();
     }
-    delay(50);
-    return;
-  }
-  else{
-    if(pantallaApagada){
-      pantallaApagada = false;
+
+  } else {
+    if (modoPanic) {
+      modoPanic = false;
       dibujarTexto();
     }
+    delay(50);
+  }
+  lastStateSwitch = buttonSwitch;
+
+// luces
+  if (modoPanic){
+    delay(20);
+    return;
   }
 
+  //botones
   bool up = digitalRead(BTN_UP);
   bool down = digitalRead(BTN_DOWN);
 
@@ -95,6 +117,7 @@ void loop() {
     if (lineaActual > 0){
       lineaActual--;
       dibujarTexto();
+      beep();
     }
 
     delay(50);
@@ -106,6 +129,7 @@ void loop() {
     if (lineaActual < (totalLines - MAXILINES)){
       lineaActual++;
       dibujarTexto();
+      beep();
     }
 
     delay(50);
@@ -114,4 +138,48 @@ void loop() {
   lastStateDown = down;
 
   delay(10);
+}
+
+void dibujarTexto(){
+  oled.clearDisplay();
+  oled.setCursor(0, 0);
+
+  for(int i = 0; i < MAXILINES; i++){
+    int indice = lineaActual + i;
+    if(indice < totalLines){
+      oled.println(fanfiction[indice]);
+    }
+  }
+
+  int luz = analogRead(AO_PHOTO);
+
+  oled.setCursor(0, 50);
+  if (luz > 500){
+    oled.println("Too much light");
+  } else {
+    oled.println("Light is ok!");
+  }
+
+ oled.display();
+}
+
+void dibujarPanic(){
+  float tempC = dht11.readTemperature();
+
+  oled.clearDisplay();
+  oled.setCursor(0, 10);
+  oled.println("--------------------");
+  oled.println("Nothing to see here!");
+  oled.println("--------------------");
+  oled.println("Temp: ");
+  oled.println(tempC);
+  oled.println("C");
+
+  oled.display();
+}
+
+void beep(){
+  digitalWrite(BUZZER, HIGH);
+  delay(30);
+  digitalWrite(BUZZER, LOW);
 }
